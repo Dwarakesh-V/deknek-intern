8c87e245092b0d278210e61c60ebf6955b024508
Dwarakesh-V <vdwarakesh0211@gmail.com>
2026-05-03 14:19:56 +0530

    Update visuals

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  appTitle: 'test',
-  appDescription: 'description',
+  appTitle: 'All in One Markdown',
+  appDescription: 'A clean and secure notes platform built for students, developers, and creators. Create notes, edit instantly, store safely, and download your files as markdown anytime.',
   logo: '/logo.png',
 };
