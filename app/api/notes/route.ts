f5fd4fe70c7d5ef7b2208448ca73da74ba9e105e
Dwarakesh-V <vdwarakesh0211@gmail.com>
2026-05-03 10:39:37 +0530

    Fix authoptions route

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next';
 import prisma from '@/app/libs/prismaDb';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/app/utils/authOptions';
 
 // ----------------------------------------------------------------------
 // GET: Fetch all notes for the logged-in user
