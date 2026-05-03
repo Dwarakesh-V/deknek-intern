eb397a276fb75c7c8fb87921acbdd8637b54b453
Dwarakesh-V <vdwarakesh0211@gmail.com>
2026-05-03 14:22:21 +0530

    Fix config

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,6 @@
 export const CONFIG = {
   appTitle: 'All in One Markdown',
-  appDescription: 'A clean and secure notes platform built for students, developers, and creators. Create notes, edit instantly, store safely, and download your files as markdown anytime.',
+  appDescription:
+    'A clean and secure notes platform built for students, developers, and creators. Create notes, edit instantly, store safely, and download your files as markdown anytime.',
   logo: '/logo.png',
 };
